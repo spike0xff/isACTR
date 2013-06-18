807b13a2a3d7ccf20a860b6649db1d1a39363997
Hugh (Spike) McLarty <spike.mclarty@gmail.com>
2013-06-17 23:55:56 -0400

    added the version.h to the project and bumped to 0.0.6.0

--- isactr/version.h
+++ isactr/version.h
@@ -3,10 +3,11 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_RELEASE 4
+#define VERSION_RELEASE 6
 #define VERSION_BUILD 0
 
 /*
+0.0.6.0	2013.06.17	spike	simple read-eval-print loop working
 0.0.5.0	2013.05.21	spike	stubs for some Lisp functions
 0.0.4.0	2013.04.30	spike	starting on production matching	
 0.0.3.0				spike	added a few asserts, changed event dequeue to return event ptr.
